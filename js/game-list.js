9e73185a7fa8d881ffbbbd45833672c18001eb9c
Anka <103377387+ann153476@users.noreply.github.com>
2023-07-18 23:59:19 +0300

    all

---
+++ js/game-list.js
@@ -0,0 +1,40 @@
+const gameImages = [
+  "vs1fortunetree",
+  "vs9chen",
+  "vs20cleocatra",
+  "vs20gatotgates",
+  "vs20nexussr",
+  "vs25tigeryear",
+  "vs25tigeryear",
+  "vs243mwarrior",
+  "vs1024mahjpanda",
+  "vswayschilheat",
+  "vswaysfrbugs",
+  "vswaysrsm",
+  "vswayssamurai",
+];
+let gameItems = document.querySelectorAll(".game-item");
+
+let imageCount = gameImages.length;
+let usedImages = [];
+function getRandomImageIndex(imageCount, usedImages) {
+  var randomIndex = Math.floor(Math.random() * imageCount);
+  if (usedImages.length === imageCount) {
+    usedImages = [];
+  }
+  while (usedImages.includes(randomIndex)) {
+    randomIndex = Math.floor(Math.random() * imageCount);
+  }
+  usedImages.push(randomIndex);
+  return randomIndex;
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < gameItems.length; i++) {
+    gameItems[i].style.height = gameItems[0].offsetWidth + "px";
+    let randomImageIndex = getRandomImageIndex(imageCount, usedImages);
+    gameItems[
+      i
+    ].style.backgroundImage = `url(../img/popular-game-list/${gameImages[randomImageIndex]}.webp)`;
+  }
+});
